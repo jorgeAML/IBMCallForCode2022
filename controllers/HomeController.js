8dc95cc4b8788dc44e56657f909e77aaf2d1a089
jorgeAML <martjorge8@gmail.com>
2022-09-07 13:48:02 -0600

    To english

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -2,14 +2,14 @@ exports.respondWithIndex = (req, res) => {
     res.render("index", {title:"IBM PROYECT 2022"});
 };
 exports.respondWithConocenos = (req, res) => {
-    res.render("conocenos", {title:"Acerca de nosotros"});
+    res.render("conocenos", {title:"About us"});
 };
 exports.respondWithServicios = (req, res) => {
-    res.render("servicios", {title:"Servicios"});
+    res.render("servicios", {title:"Services"});
 };
 exports.respondWithContact = (req, res) => {
-    res.render("contacto", {title:"contactanos"});
+    res.render("contacto", {title:"contact"});
 };
 exports.respondWithCurses = (req, res) => {
-    res.render("cursos", {title: "Cursos de reciclaje"});
+    res.render("cursos", {title: "Recycle curse"});
 };
