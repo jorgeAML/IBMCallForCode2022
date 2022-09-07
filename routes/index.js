fa5771dba729510b395cf3756516a008b592bc20
jorgeAML <martjorge8@gmail.com>
2022-09-06 19:27:15 -0600

    prueba static

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const homeController = require('../controllers/HomeController');
+
 router.get('/', homeController.respondWithIndex);
 
 module.exports = router;
