e1abf3281d4cbe371c4ffdb9a3b637e8c403e7db
jorgeAML <martjorge8@gmail.com>
2022-10-04 20:33:48 -0600

    all for admins and collections updated

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,4 +1,5 @@
 const EasyRecycle = require('../models/recycle');
+const FindMyTrash = require('../models/findTrashModel');
 const User = require('../models/users');
 
 module.exports = {
@@ -63,5 +64,60 @@ module.exports = {
                 console.log(`Error deleting recycle values by ID ${error.message}`);
                 next(error);
             });
-    }
+    },
+    //GARBAGE POINTS
+    garbageFind: (req, res, next) => {
+        FindMyTrash.find({})
+            .then(findmytrashes => {
+            res.locals.findmytrashes = findmytrashes;
+            next();
+        }).catch(error => {
+            console.log(`Error fetching findmytrashes collection ${error.message}`);
+            next(error);
+        });
+    },
+    garbageEdit: (req, res, next) => {
+        let easyID = req.params.id;
+        FindMyTrash.findById(easyID)
+            .then(findmytrashes => {
+                res.render("admin/editGarbage", {
+                    findmytrashes: findmytrashes,
+                    title: "Edit values for easyRecycle section"
+                }); 
+            }).catch(error => {
+                console.log(`Error fetching values by ID ${error.message}`);
+            });
+    },
+    garbageUpdate: (req, res, next) => {
+        let easyID = req.params.id,
+            recycleParams = {
+                name: req.body.name,
+                phone: req.body.phone,
+                address: req.body.address,
+                items: req.body.items,
+                description: req.body.description,
+                date: req.body.date
+            };
+            FindMyTrash.findByIdAndUpdate(easyID, {
+            $set: recycleParams
+        }).then(findmytrashes => {
+            res.locals.redirect = '/admin';
+            res.locals.findmytrashes = findmytrashes;
+            next();
+        }).catch(error => {
+            console.log(`Error updating recycle values by ID: ${error.message}`);
+            next(error);
+        });
+    },
+    garbageDelete: (req, res, next) => {
+        let easyID = req.params.id;
+        FindMyTrash.findByIdAndRemove(easyID)
+            .then(() => {
+                res.locals.redirect = '/admin';
+                next();
+            }).catch (error => {
+                console.log(`Error deleting recycle values by ID ${error.message}`);
+                next(error);
+            });
+    },
 }
--- routes/admin.js
+++ routes/admin.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const adminController = require('../controllers/adminController');
 
-router.get('/admin', adminController.indexReycleForAdmin ,adminController.indexViewRecycleForAdmin);
+router.get('/admin', adminController.indexReycleForAdmin,adminController.garbageFind ,adminController.indexViewRecycleForAdmin);
 router.get('/admin/:id/edit', adminController.edit);
-router.put('/admin/:id/update', adminController.update, adminController.redirectView);
-router.delete('/admin/:id/delete', adminController.delete, adminController.redirectView);
+router.get('/admin/:id/editGarbage', adminController.garbageEdit);
+router.put('/admin/:id/update', adminController.update,adminController.garbageUpdate, adminController.redirectView);
+router.delete('/admin/:id/delete', adminController.delete, adminController.garbageDelete, adminController.redirectView);
 
 module.exports = router;
